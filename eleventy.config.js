e9fc80297769c15db01774436a71f8143c20cf37
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2023-02-06 10:30:50 -0700

    initial implementation of basic 11ty template modeled on WP2030

---
+++ eleventy.config.js
@@ -0,0 +1,49 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const markdownItAnchor = require("markdown-it-anchor");
+const markdownItAttrs = require("markdown-it-attrs");
+const yaml = require("js-yaml");
+const { parse } = require('csv-parse/sync');
+const charts = require('eleventy-charts');
+
+module.exports = function(eleventyConfig) {
+  const pathPrefix = process.env.BASEURL || "/";
+
+  // Copy assets directory
+  eleventyConfig.addPassthroughCopy("assets");
+
+  // Add 11ty plugins
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  eleventyConfig.addPlugin(charts);
+
+  // Add markdown-it plugins
+  eleventyConfig.amendLibrary("md", md => md.use(markdownItAnchor));
+  eleventyConfig.amendLibrary("md", md => md.use(markdownItAttrs));
+
+  // Read YAML files in the _data directory
+  eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+
+  // Read CSV files in the _data directory
+  eleventyConfig.addDataExtension("csv", (contents) => {
+    const records = parse(contents, {
+      columns: true,
+      skip_empty_lines: true,
+    });
+    return records;
+  });
+
+  // Watch for changes in additional directories
+  eleventyConfig.addWatchTarget("_data");
+
+  // Make these variables available everywhere on the site
+  eleventyConfig.addGlobalData("pathPrefix", pathPrefix);
+
+  // Add a comment shortcode
+  // eleventyConfig.addPairedShortcode("comment", () => {});
+
+  return {
+    pathPrefix,
+  };
+};
