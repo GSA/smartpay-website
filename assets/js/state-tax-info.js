1caf7df450636a629fe113525c99f8c2e8e945b4
weiwang-gsa <wei.wang@gsa.gov>
2023-02-24 17:04:53 -0500

    add states, adjust code to fit state difference

--- assets/js/state-tax-info.js
+++ assets/js/state-tax-info.js
@@ -1,16 +1,10 @@
-
-const dropdown = document.getElementById("ddlstates");
-
-// add a change event listener to the dropdown menu
-dropdown.addEventListener("change", function() {
-
-  // get the value of selected dropdown
+function showstate()
+{
+  const dropdown = document.getElementById("ddlstates");
   const selectedOption = dropdown.value;
 
   // get all the div elements with the hidden class
   const hideableDivs = document.querySelectorAll(".hidden-items");
-
-  // loop through and find the div id matching with drop down selection, if match found display it, otherwise, hide it
   hideableDivs.forEach(function(div) {
 
     if (div.id === selectedOption) {
@@ -21,4 +15,4 @@ dropdown.addEventListener("change", function() {
     
   });
 
-});
+}
