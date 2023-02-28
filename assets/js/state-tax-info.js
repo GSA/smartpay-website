bd436088e4d07fdf0f3812df6d818aadc4a2da6f
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2023-02-27 17:20:01 -0700

    navigate only if an actual state path is selected

--- assets/js/state-tax-info.js
+++ assets/js/state-tax-info.js
@@ -2,6 +2,8 @@ function showstate()
 {
   const dropdown = document.getElementById("ddlstates");
   const selectedStatePath = dropdown.value;
-  const target = window.location.protocol + "//" + window.location.host + selectedStatePath;
-  window.location.href = target;
+  if (selectedStatePath) {
+    const target = window.location.protocol + "//" + window.location.host + selectedStatePath;
+    window.location.href = target;
+  }
 }
