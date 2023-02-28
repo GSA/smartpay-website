237db9ad03ccee0f828160d8da6ed9136b0c7244
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2023-02-27 18:01:48 -0700

    use a list of states rather than a dropdown

--- assets/js/state-tax-info.js
+++
@@ -1,9 +0,0 @@
-function showstate()
-{
-  const dropdown = document.getElementById("ddlstates");
-  const selectedStatePath = dropdown.value;
-  if (selectedStatePath) {
-    const target = window.location.protocol + "//" + window.location.host + selectedStatePath;
-    window.location.href = target;
-  }
-}
