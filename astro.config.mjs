238df55560084c94eaec2d94350a8640fa2f67e0
Mark Meyer <mark@photo-mark.com>
2023-06-23 14:50:07 -0800

    experiment to test why pages is not loading static assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import uswds_links from "./src/plugins/uswds_links";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://training.smartpay.gsa.gov',
+    site: 'https://smartpay.gsa.gov',
     base: process.env.BASEURL,
     integrations: [mdx()],
     outDir: '_site',
