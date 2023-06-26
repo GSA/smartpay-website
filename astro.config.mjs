41080517128b4671c41cf9f075ec635aa514ff7c
Mark Meyer <mark@photo-mark.com>
2023-06-26 10:06:18 -0800

    add sitemap and config for pa11y tests

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import uswds_links from "./src/plugins/uswds_links";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://smartpay.gsa.gov',
     base: process.env.BASEURL,
-    integrations: [mdx()],
+    integrations: [mdx(), sitemap()],
     outDir: '_site',
     markdown: {
       rehypePlugins: [uswds_links]
