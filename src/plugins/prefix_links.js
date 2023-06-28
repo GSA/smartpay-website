4ccf16baba258d9c4385368afc122ba98e830ebc
Mark Meyer <mark@photo-mark.com>
2023-06-28 13:32:51 -0800

    use node's path to help normalize path differences

--- src/plugins/prefix_links.js
+++ src/plugins/prefix_links.js
@@ -1,22 +1,20 @@
 import {visit} from "unist-util-visit"
+import path from 'path'
 
 export default (options) => {
   function visitor(node) {
-    // only process links that start at the root
-    // 
-    if (node.url.startsWith('/')) {
-        
-        node.url = `${options.baseURL}${node.url}`
+    // only process links that start at the root without schema
+    if (node.url.startsWith('/') && !node.url.startsWith('//')) {
+        node.url = path.join('/', options.baseURL, node.url)
     }
   }
 
   function transform(tree) {
-    if (options && options.baseURL) {
+    if (options) {
       visit(tree, "link", visitor)
     } else {
       throw Error("Missing required `baseURL` option.")
     }
   }
-
   return transform
 }
