086df51f1eca3faded3b3cdc74f066147d079d5b
weiwang-gsa <wei.wang@gsa.gov>
2023-08-02 12:36:52 -0400

    code clean up, add coments to componets

--- src/assets/js/faqHelper.js
+++ src/assets/js/faqHelper.js
@@ -1,3 +1,4 @@
+// JS function used in FAQ page, it will expand all collapsed accordions when user perform browser search
 export function handleAccordionSearch(){
 
     function expandAllAccordions(){
@@ -7,42 +8,15 @@ export function handleAccordionSearch(){
         })
         var accordionContents = document.querySelectorAll('.sd-accordion-js');
         accordionContents.forEach((content)=>{
-            //alert(content.innerHTML)
             content.removeAttribute('hidden');
         });
 
     }
     window.addEventListener("keydown", function(e) {
-        // when user press cmd+f or ctrl + f for search, make all accordion expanded for search
-    
+        // when user press cmd+f or ctrl + f for search, make all accordions expanded
         if ((e.ctrlKey || e.metaKey) && e.key === 'f') {
             expandAllAccordions();
         }
     })
 }
-    // // below JS is to increase original heading level from the FAQ collection because addtional H1 tag Frequently Asked Qunestions is added. 
-    export function increaseHeadingLevels(){
-        const headings = document.querySelectorAll('h1, h2, h3, h4,h5');
-        headings.forEach(heading =>{
-            if(heading.innerHTML !="Frequently Asked Questions")
-            {
-                const currentTagName = heading.tagName;
-                const currentLevel = parseInt(currentTagName.substring(1))
-                const newLevel = currentLevel + 1; 
-                const newTageName = `H${newLevel}`
-                const newHeading = document.createElement(newTageName);
-                newHeading.innerHTML = heading.innerHTML;
-                Array.from(heading.attributes).forEach(attr=>{
-                    newHeading.setAttribute(attr.name,attr.value);
-                })
-                heading.parentNode.replaceChild(newHeading,heading);
-            }
-        })
-    }
-
-// function to  generate dynamic id
-let counter = 0;
-export function generateUniqueId(type){
-    return `${type}-${counter ++}`
-}
-
+ 
