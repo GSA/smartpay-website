0bf401958ad9d16c48b52fdd560b61276957746d
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2023-08-08 07:50:29 -0600

    filter out redirects when building sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import process_anchors from "./src/plugins/process_anchors";
 import process_image_urls from './src/plugins/process_image_urls';
 import sitemap from "@astrojs/sitemap";
 import redirects from "./src/config/redirects";
+import sitemapFilter from "./src/config/sitemapFilter";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://federalist-ab31a10d-375d-4040-9324-1ae94e8a36b9.sites.pages.cloud.gov/site/gsa/smartpay-website/',
     base: process.env.BASEURL,
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap({ filter: sitemapFilter })],
     outDir: '_site',
     markdown: {
       rehypePlugins: [
