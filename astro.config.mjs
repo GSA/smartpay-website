b9b8a52d221d575366ce2a876dbdeccaaf6f04c4
Dan Felder <d.felder@kunzleigh.com>
2024-05-21 12:01:51 -0400

    Update astro library

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,9 +14,10 @@ export default defineConfig({
     outDir: '_site',
     markdown: {
       rehypePlugins: [
-        [process_anchors, {baseURL: process.env.BASEURL || '/'}],
-        [process_image_urls, {baseURL: process.env.BASEURL || '/'}]
+        [process_anchors, {baseURL: process.env.BASEURL}],
+        [process_image_urls, {baseURL: process.env.BASEURL}]
       ],
     },
-    redirects: generateRedirects(process.env.BASEURL)
+    redirects: generateRedirects(process.env.BASEURL),
+    trailingSlash: 'always',
 });
